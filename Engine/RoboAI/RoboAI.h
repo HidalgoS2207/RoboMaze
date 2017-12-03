34739f8815d1e2a99dc354c4fd72dca974befbc9
Sebastián Hidalgo <sebahidalgo@live.com.ar>
2017-12-03 14:21:03 -0300

    Add files via upload

--- Engine/RoboAI/RoboAI.h
+++ Engine/RoboAI/RoboAI.h
@@ -4,30 +4,182 @@
 #include <random>
 #include <deque>
 
-// simple demo that randomly turns when it runs into a wall
+
 class RoboAI_Demo
 {
 	using TT = TileMap::TileType;
 	using Action = Robo::Action;
 public:
-	Action Plan( std::array<TT,3> view )
+	Action Plan(std::array<TT, 3> view)
 	{
-		if( view[1] == TT::Wall )
+		if (!winner)
 		{
-			if( coinflip( rng ) )
+			//two step movement check
+			if (do_next_move && !(std::any_of(view.begin(), view.end(), [](auto a) {return a == TT::Goal; })))
 			{
-				return Action::TurnRight;
+				do_next_move = false;
+
+				stm = { view[0],view[1],view[2] };
+
+				last_move = next_move;
+				return next_move;
 			}
-			else
+
+			//if no accesible exit at sight do generic checks and movement, else, move towards exit (after accesible exit check)
+			if (!exit_at_sight)
 			{
-				return Action::TurnLeft;
+				//check if 'exit at sight' (and if the dude have at least one memory of the path behing)
+				if (std::any_of(view.begin(), view.end(), [](auto a) {return a == TT::Goal; }))
+				{
+					if (first_movement) //could be a trap, must to check
+					{
+						first_movement = false;
+
+						if (view[1] == TT::Goal)
+						{
+							//lucky dude
+							exit_at_sight = true;
+						}
+						else
+						{
+							//kickoff movement because we can't be sure if exit is accesible
+							stm = { view[0],view[1],view[2] };
+
+							if (view[0] == TT::Goal)
+							{
+								last_move = Action::TurnLeft;
+								return Action::TurnLeft;
+							}
+							else
+							{
+								last_move = Action::TurnRight;
+								return Action::TurnRight;
+							}
+						}
+					}
+					else
+					{
+						//standard 'exit at sight' verification
+						if (view[1] != TT::Wall)
+						{
+							exit_at_sight = true;
+						}
+						else
+						{
+							if (view[2] == TT::Goal)
+							{
+								if (last_move == Action::TurnLeft)
+								{
+									if (stm[1] == TT::Wall)
+									{
+										stm = { view[0],view[1],view[2] };
+
+										last_move = Action::TurnLeft;
+										return Action::TurnLeft;
+									}
+									else
+									{
+										exit_at_sight = true;
+									}
+								}
+								
+							}
+							else if(view[0] == TT::Goal)
+							{
+								if (last_move == Action::TurnRight)
+								{
+									if (stm[0] == TT::Wall)
+									{
+										stm = { view[0],view[1],view[2] };
+
+										last_move = Action::TurnRight;
+										return Action::TurnRight;
+									}
+									else
+									{
+										exit_at_sight = true;
+									}
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					if (first_movement) first_movement = false;
+
+					stm = { view[0],view[1],view[2] };
+
+					//standard movement
+					if (view[2] == TT::Floor) //Checks if turn to right is allowed
+					{
+						if (view[1] != TT::Wall) //if a wall is in the face of the dude, must be an aisle or only left turn available
+						{
+							do_next_move = true;
+
+							next_move = Action::TurnRight;
+
+							last_move = Action::MoveForward;
+							return Action::MoveForward;
+						}
+						else
+						{
+							last_move = Action::TurnLeft;	
+							return Action::TurnLeft; //exceptional case must to find a way
+						}
+					}
+					else //no turning right allowed
+					{
+						if (view[1] == TT::Floor) //continue straight if possible
+						{
+							last_move = Action::MoveForward;
+							return Action::MoveForward;
+						}
+						else //must be possible to turn left and see what happens
+						{
+							last_move = Action::TurnLeft;
+							return Action::TurnLeft;
+						}
+					}
+				}
+			}
+			
+			stm = { view[0],view[1],view[2] };
+			
+			if(exit_at_sight)
+			{
+				//moving towards exit
+				int i = 0;
+				for (; view[i] != TT::Goal ; i++);
+
+				switch (i)
+				{
+				case 0:
+					next_move = Action::TurnLeft;
+					do_next_move = true;
+					return Action::MoveForward;
+				case 1:
+					winner = true;
+					return Action::MoveForward;
+				case 2:
+					next_move = Action::TurnRight;
+					do_next_move = true;
+					return Action::MoveForward;
+				}
 			}
 		}
-		return Action::MoveForward;
+		
+		return Action::Done;
 	}
 private:
-	std::mt19937 rng = std::mt19937( std::random_device{}() );
-	std::bernoulli_distribution coinflip;
+	Action next_move;
+	Action last_move;
+	bool do_next_move = false;
+	bool exit_at_sight = false;
+	bool winner = false;
+	bool first_movement = true;
+
+	std::array<TT, 3> stm; //dude's short-term memory
 };
 
 // use this if you don't want to implement a debug RoboAI
@@ -57,8 +209,7 @@ public:
 		:
 		dc( dc )
 	{}
-	// this signals to the system whether the debug AI can be used
-	static constexpr bool implemented = true;
+	static constexpr bool implemented = false;
 	Action Plan( std::array<TT,3> view )
 	{
 		if( view[1] == TT::Wall )
